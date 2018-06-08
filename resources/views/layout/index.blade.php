b88ca003f6ea14f859cd9e7327de2dff5e4fe0d2
charly18 <charleskouamekouadio@gmail.com>
2018-06-08 18:17:35 +0000

    charles

--- resources/views/layout/index.blade.php
+++ resources/views/layout/index.blade.php
@@ -387,7 +387,7 @@
            @yield('appContent')
             <!-- End Container fluid  -->
             <!-- footer -->
-            <footer class="footer"> © 2018 All rights reserved. Template designed by <a href="https://colorlib.com">Colorlib</a></footer>
+            <footer class="footer"> © 2018 All rights reserved <a href="https://colorlib.com">Colorlib</a></footer>
             <!-- End footer -->
         </div>
         <!-- End Page wrapper  -->
